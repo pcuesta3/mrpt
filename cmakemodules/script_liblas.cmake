fd6261bacb388aa676117d54cac00fa92656baf3
fcuesta <fcuesta@HWPO0147-Workstation.(none)>
2015-01-15 13:44:36 +0100

    Leave at the user's choice to disable the LAS lib

--- cmakemodules/script_liblas.cmake
+++ cmakemodules/script_liblas.cmake
@@ -1,52 +1,57 @@
 # Check for the LAS (LiDAR format) library
 # ===================================================
-IF(UNIX)
-	FIND_FILE(LAS_CONFIG_FILE liblas-config)
-	IF(LAS_CONFIG_FILE)
-		MARK_AS_ADVANCED(LAS_CONFIG_FILE)
 
-		SET(CMAKE_MRPT_HAS_LIBLAS 1)
-		SET(CMAKE_MRPT_HAS_LIBLAS_SYSTEM 1)
+SET(CMAKE_MRPT_HAS_LIBLAS 0)
 
-		# Get the config params:
-		EXECUTE_PROCESS(COMMAND ${LAS_CONFIG_FILE} --libs
-			RESULT_VARIABLE CMAKE_LAS_CONFIG_RES
-			OUTPUT_VARIABLE CMAKE_LAS_LIBS
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-			)
-		IF(${CMAKE_LAS_CONFIG_RES})
-			MESSAGE("Error invoking LAS config file:\n ${LAS_CONFIG_FILE} --libs")
-		ENDIF(${CMAKE_LAS_CONFIG_RES})
+# Leave at the user's choice to disable the LAS lib:
+OPTION(DISABLE_LIBLAS "Forces NOT using LAS lib, even if it could be found by CMake" "OFF")
+MARK_AS_ADVANCED(DISABLE_LIBLAS)
 
-		EXECUTE_PROCESS(COMMAND ${LAS_CONFIG_FILE} --includes
-			RESULT_VARIABLE CMAKE_LAS_CONFIG_RES
-			OUTPUT_VARIABLE CMAKE_LAS_INCLUDES
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-			)
-		IF(${CMAKE_LAS_CONFIG_RES})
-			MESSAGE("Error invoking LAS config file:\n ${LAS_CONFIG_FILE} --includes")
-		ENDIF(${CMAKE_LAS_CONFIG_RES})
+IF(NOT DISABLE_LIBLAS)
+   # Windows: (Not supported for now)
+   IF(UNIX)
+      FIND_FILE(LAS_CONFIG_FILE liblas-config)
+      IF(LAS_CONFIG_FILE)
+         MARK_AS_ADVANCED(LAS_CONFIG_FILE)
 
-		# Join all flags and parse to separate them:
-		SET(CMAKE_LAS_CFGS "${CMAKE_LAS_LIBS} ${CMAKE_LAS_INCLUDES}")
+         SET(CMAKE_MRPT_HAS_LIBLAS 1)
+         SET(CMAKE_MRPT_HAS_LIBLAS_SYSTEM 1)
 
-		pkgconfig_parse(${CMAKE_LAS_CFGS} "LAS")
+         # Get the config params:
+         EXECUTE_PROCESS(COMMAND ${LAS_CONFIG_FILE} --libs
+            RESULT_VARIABLE CMAKE_LAS_CONFIG_RES
+            OUTPUT_VARIABLE CMAKE_LAS_LIBS
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+         IF(${CMAKE_LAS_CONFIG_RES})
+            MESSAGE("Error invoking LAS config file:\n ${LAS_CONFIG_FILE} --libs")
+         ENDIF(${CMAKE_LAS_CONFIG_RES})
 
-		# For some reason, "liblas-config --libs" return all other libs, except liblas itself:
-		LIST(APPEND LAS_LIBS "las")
+         EXECUTE_PROCESS(COMMAND ${LAS_CONFIG_FILE} --includes
+            RESULT_VARIABLE CMAKE_LAS_CONFIG_RES
+            OUTPUT_VARIABLE CMAKE_LAS_INCLUDES
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+         IF(${CMAKE_LAS_CONFIG_RES})
+            MESSAGE("Error invoking LAS config file:\n ${LAS_CONFIG_FILE} --includes")
+         ENDIF(${CMAKE_LAS_CONFIG_RES})
 
-		IF($ENV{VERBOSE})
-			MESSAGE(STATUS "liblas configuration:")
-			MESSAGE(STATUS "  LAS_INCLUDE_DIRS: ${LAS_INCLUDE_DIRS}")
-			MESSAGE(STATUS "  LAS_LINK_DIRS: ${LAS_LINK_DIRS}")
-			MESSAGE(STATUS "  LAS_LIBS: ${LAS_LIBS}")
-		ENDIF($ENV{VERBOSE})
+         # Join all flags and parse to separate them:
+         SET(CMAKE_LAS_CFGS "${CMAKE_LAS_LIBS} ${CMAKE_LAS_INCLUDES}")
 
-	ELSE(LAS_CONFIG_FILE)
-		SET(CMAKE_MRPT_HAS_LIBLAS 0)
-	ENDIF(LAS_CONFIG_FILE)
-ELSE(UNIX)
-	# Windows: (Not supported for now)
-	SET(CMAKE_MRPT_HAS_LIBLAS 0)
-ENDIF(UNIX)
+         pkgconfig_parse(${CMAKE_LAS_CFGS} "LAS")
+
+         # For some reason, "liblas-config --libs" return all other libs, except liblas itself:
+         LIST(APPEND LAS_LIBS "las")
+
+         IF($ENV{VERBOSE})
+            MESSAGE(STATUS "liblas configuration:")
+            MESSAGE(STATUS "  LAS_INCLUDE_DIRS: ${LAS_INCLUDE_DIRS}")
+            MESSAGE(STATUS "  LAS_LINK_DIRS: ${LAS_LINK_DIRS}")
+            MESSAGE(STATUS "  LAS_LIBS: ${LAS_LIBS}")
+         ENDIF($ENV{VERBOSE})
+
+      ENDIF(LAS_CONFIG_FILE)
+   ENDIF(UNIX)
+ENDIF(NOT DISABLE_LIBLAS)
 
